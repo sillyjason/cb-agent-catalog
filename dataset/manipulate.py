1232d976eca81c12454f2e00039cb6ca054e8d2c
sillyjason <sillyjason.cao@gmail.com>
2024-12-27 14:48:38 +0800

    wip

--- dataset/manipulate.py
+++ dataset/manipulate.py
@@ -14,7 +14,7 @@ def update_products_json(file_path):
     updated_issues = []
     for index, issue in enumerate(issues):
         # Remove the manufacturer and product_id fields
-        updated_image_path = issue['image_path'] + ".png"
+        updated_image_path = "./" + issue['image_path'] 
         concatenate = f"{issue['issue_id'] } *** {updated_image_path}  *** {issue['description']}" 
         issue['concatenate'] = concatenate
         issue['image_path'] = updated_image_path
@@ -26,3 +26,17 @@ def update_products_json(file_path):
 # Update the products.json file
 file_path = '/Users/jc/Desktop/OtherTechies/Python_LangChain/agentic_customer_service_with_agentc/dataset/defects.json'
 update_products_json(file_path)
+
+
+# # Define the directory containing the images
+# directory = './static/images/defects_images/'
+
+# # Loop through the files in the directory
+# for i, filename in enumerate(sorted(os.listdir(directory))):
+#     # Construct the old and new file paths
+#     old_file = os.path.join(directory, filename)
+#     new_file = os.path.join(directory, f"{i}.png")
+    
+#     # Rename the file
+#     os.rename(old_file, new_file)
+#     print(f"Renamed {old_file} to {new_file}")
