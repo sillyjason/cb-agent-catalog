4747a74ec3db0b76ddda68bba2624bebf4712433
sillyjason <sillyjason.cao@gmail.com>
2025-03-05 01:09:12 +0800

    wip

--- agentic/embedding.py
+++ agentic/embedding.py
@@ -8,6 +8,6 @@ model = SentenceTransformer(model_path)
 # Generate embedding
 def create_embeddings(input_message):
     embeddings = model.encode([input_message])[0]
-    # Convert embeddings to a list of floats
+    # Coembenvert embeddings to a list of floats
     embeddings_list = embeddings.tolist()
     return embeddings_list
